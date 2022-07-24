3ea5cf6232c1bfc909832e574d36407f8e449442
subhashmeena <subhashmeena729@gmail.com>
2022-07-24 10:02:44 +0200

    Initialized repository

---
+++ src/test/java/dev/subhashmeena/utilities/mtlsping/ScalaMtlsPingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.subhashmeena.utilities.mtlsping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalaMtlsPingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
